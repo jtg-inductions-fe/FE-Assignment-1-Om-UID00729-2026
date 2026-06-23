9f9699d0dfc0923d10e219e950ddcb60b16aa3d3
Om Handa <om.handa@joshtechnologygroup.com>
2026-06-23 11:48:33 +0530

    [OH_A1_01] Header: Worked on responsiveness

--- src/script/index.js
+++ src/script/index.js
@@ -1,3 +1,7 @@
 import handleHamburgerClick from './navbar.js';
 
+<<<<<<< HEAD
 handleHamburgerClick();
+=======
+handleHamburgerClick();
+>>>>>>> 27b251c ([OH_A1_01] Header: Worked on responsiveness)
