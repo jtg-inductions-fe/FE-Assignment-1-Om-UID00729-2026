87d442e4e587091274c9e178106849896e4025bf
Om Handa <om.handa@joshtechnologygroup.com>
2026-06-26 12:46:46 +0530

    [OH_A1_01] Header Section: Merge Conflicts Resolved

--- src/script/index.js
+++ src/script/index.js
@@ -1,11 +1,3 @@
 import handleHamburgerClick from './navbar.js';
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 handleHamburgerClick();
-=======
-handleHamburgerClick();
->>>>>>> 27b251c ([OH_A1_01] Header: Worked on responsiveness)
-=======
-handleHamburgerClick();
->>>>>>> dd034bf ([OH_A1_01] Header Section- Updated according to change in base setup)
