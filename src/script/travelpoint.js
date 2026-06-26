260a2f5baa3926cea1c547ebd74c5a251aed95b0
Om Handa <om.handa@joshtechnologygroup.com>
2026-06-26 19:04:22 +0530

    [OH_A1_03] Travel Point section: Layout Created

---
+++ src/script/travelpoint.js
@@ -0,0 +1,24 @@
+let detailsCont = document.querySelector('.travelpoint__textSide__details');
+
+let data = [
+    { numbers: '500+', content: 'Holiday Package' },
+    { numbers: '100', content: 'Luxury Hotel' },
+    { numbers: '7', content: 'Premium Airlines' },
+    { numbers: '2k+', content: 'Happy Customers' },
+];
+
+let renderData = () => {
+    let layout = data
+        .map((ele) => {
+            return `
+                <div class="travelpoint__textSide__details__cont">
+                    <h4>${ele.numbers}</h4>   
+                    <p>${ele.content}</p>
+                </div>
+            `;
+        })
+        .join('');
+    detailsCont.innerHTML = layout;
+};
+
+export default renderData;
