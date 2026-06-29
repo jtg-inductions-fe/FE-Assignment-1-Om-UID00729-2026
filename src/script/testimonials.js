5622a28b613a916d2e3f5bd065edecfc71649930
Om Handa <om.handa@joshtechnologygroup.com>
2026-06-29 11:18:10 +0530

    [OH_A1_04] Testimonial Section: Pixel Perfect and responsiveness

--- src/script/testimonials.js
+++ src/script/testimonials.js
@@ -68,7 +68,7 @@ testimonials.innerHTML += layout.join(' ');
 const splide = new Splide('.splide', {
     type: 'loop',
     perPage: 1,
-    autoplay: true,
+    // autoplay: true,
     pagination: true,
     arrows: false,
 });
