859f26c55976fcdc8bedec4fe1876b2b44df8b81
omhanda-jtg <om.handa@joshtechnologygroup.com>
2026-06-30 20:58:32 +0530

    OH_A1_01 Header Section: Developed a responsive header with working hamburger menu, accessibility features and fixed navbar.
    * [OH_A1_01] Header Section: Created Structure for header
    
    * [OH_A1_01] Header: Worked on responsiveness
    
    * [OH_A1_01] Header Section- Updated according to change in base setup
    
    * [OH_A1_00] Base Setup: Review Fixes, typography updated, added icons from icomoon, barrel files
    
    * [OH_A1_00] Base Setup - Review fixes: typograpphy and font-face
    
    * [OH_A1_00] Base setup: Review Fixes: reset updated for padding
    
    * [OH_A1_01] Header Section: Created Structure for header
    
    * [OH_A1_01] Header: Worked on responsiveness
    
    * [OH_A1_01] Header Section - Review Fixes, tab navigation added
    
    * [OH_A1_01] Header Section - Review Fixes, tab navigation added
    
    * [OH_A1_01] Confilcts solved and files updated
    
    * [OH_A1_01] Corrected main.scss file
    
    * [OH_A1_00] Base Setup: Review fixes - naming updated
    
    * [OH_A1_00] Base Setup: Review fixes - units updated for fonts and padding
    
    * [OH_A1_00] Base Setup: Review fixes - Icomoon CSS and fonts updated, favicon added, README.md updated
    
    * [OH_A1_00] Base Setup: Review fixes - Icomoon CSS and fonts updated, favicon added, README.md updated
    
    * [OH_A1_01] Header Section: Created Structure for header
    
    * [OH_A1_01] Header: Worked on responsiveness
    
    * [OH_A1_01] Header Section- Updated according to change in base setup
    
    * [OH_A1_00] Base Setup - Review fixes: typograpphy and font-face
    
    * [OH_A1_01] Header Section: Created Structure for header
    
    * [OH_A1_01] Header: Worked on responsiveness
    
    * [OH_A1_01] Header Section - Review Fixes, tab navigation added
    
    * [OH_A1_01] Header Section: Units fixing and rebased to base setup
    
    * [OH_A1_01] Header Section: Merge Conflicts Resolved
    
    * [OH_A1_00] Base Setup - Review fixes: typograpphy and font-face
    
    * [OH_A1_00] Base Setup: Review fixes - naming updated
    
    * [OH_A1_00] Base Setup: Review fixes - Icomoon CSS and fonts updated, favicon added, README.md updated
    
    * [OH_A1_01] Header Section: Created Structure for header
    
    * [OH_A1_01] Header: Worked on responsiveness
    
    * [OH_A1_01] Header Section- Updated according to change in base setup
    
    * [OH_A1_01] Header Section: Created Structure for header
    
    * [OH_A1_01] Header: Worked on responsiveness
    
    * [OH_A1_01] Header Section - Review Fixes, tab navigation added
    
    * [OH_A1_01] Confilcts solved and files updated
    
    * [OH_A1_01] Header Section: Created Structure for header
    
    * [OH_A1_01] Header: Worked on responsiveness
    
    * [OH_A1_01] Header Section- Updated according to change in base setup
    
    * [OH_A1_01] Header Section: Created Structure for header
    
    * [OH_A1_01] Header: Worked on responsiveness
    
    * [OH_A1_01] Header Section - Review Fixes, tab navigation added
    
    * [OH_A1_01] Header Section: Units fixing and rebased to base setup
    
    * [OH_A1_01] Header Section: Merge Conflicts Resolved
    
    * [OH_A1_01] Header Section: Merge Conflicts Resolved
    
    * [OH_A1_01] Header Section: Merge Conflicts Resolved
    
    * [OH_A1_01] Header Section: Update main.scss
    
    * [OH_A1_01] Header Section: Navbar links corrected
    
    * [OH_A1_01] Header Section: Review fixes, Js file updated, CSS corrected
    
    * [OH_A1_01] Header Section: review fixes
    
    * [OH_A1_01] Header Section: added title and corrected scss warning
    
    * [OH_A1_01] Header Section: added title and corrected scss warning
    
    * [OH_A1_01] Header Section: Review Fixes: naming, scroll, z-index
    
    * [OH_A1_01] Header Section: Review Fixes: Corrected scroll for navbar when hamburger is opened
    
    * [OH_A1_01] Header Section: Fixed navbar, throttle for scroll and hamburger event handling
    
    * [OH_A1_01] Header Section: Fixed reviews - updated button-text mixin, updated javascript conditions, cleaned html
    
    * [OH_A1_01] Header Section: Fixed reviews - updated base setup - colors, variables, typography

---
+++ src/script/navbar.js
@@ -0,0 +1,116 @@
+import { MOBILE, TABLET } from './constants.js';
+
+const HamburgerBtn = document.querySelector('.navbar__hamburger-btn');
+const navbarWrapper = document.querySelector('.navbar__wrapper');
+const navbarList = document.querySelector('.navbar__list');
+const hamburgerBars = document.querySelector('.icon-bars-solid');
+const navbarFocusList = document.querySelectorAll('.navbar__focus-tab');
+let lastWidth = getWidth();
+
+const isExpanded = () =>
+    navbarWrapper.classList.contains('navbar__wrapper--open') ||
+    navbarList.classList.contains('navbar__list--open');
+
+const toggleNav = () => {
+    if (window.innerWidth <= MOBILE) {
+        navbarWrapper.classList.toggle('navbar__wrapper--open');
+    } else if (window.innerWidth <= TABLET) {
+        navbarList.classList.toggle('navbar__list--open');
+    }
+
+    hamburgerBars.classList.toggle('navbar__hamburger-btn-bars--open');
+    HamburgerBtn.setAttribute('aria-expanded', String(isExpanded()));
+    HamburgerBtn.title = isExpanded()
+        ? 'Close navigation menu'
+        : 'Open navigation menu';
+    HamburgerBtn.setAttribute(
+        'aria-label',
+        isExpanded() ? 'Close navigation menu' : 'Open navigation menu',
+    );
+};
+
+function getWidth() {
+    if (window.innerWidth <= MOBILE) return 'mobile';
+    if (window.innerWidth <= TABLET) return 'tablet';
+    return 'desktop';
+}
+
+const closeNav = () => {
+    if (isExpanded()) {
+        navbarWrapper.classList.remove('navbar__wrapper--open');
+        navbarList.classList.remove('navbar__list--open');
+        hamburgerBars.classList.remove('navbar__hamburger-btn-bars--open');
+        HamburgerBtn.setAttribute('aria-expanded', 'false');
+    }
+};
+
+const classReload = () => {
+    const currWidth = getWidth();
+
+    if (currWidth !== lastWidth) {
+        closeNav();
+        lastWidth = currWidth;
+    }
+};
+
+const focusTab = () => {
+    HamburgerBtn.addEventListener('keydown', (e) => {
+        if (isExpanded()) {
+            if (e.key === 'Tab' && !e.shiftKey) {
+                e.preventDefault();
+                navbarFocusList[0].focus();
+            }
+
+            if (e.key === 'Tab' && e.shiftKey) {
+                e.preventDefault();
+                navbarFocusList[navbarFocusList.length - 1].focus();
+            }
+        }
+    });
+
+    navbarFocusList[navbarFocusList.length - 1].addEventListener(
+        'keydown',
+        (e) => {
+            if (isExpanded() && e.key === 'Tab' && !e.shiftKey) {
+                e.preventDefault();
+                HamburgerBtn.focus();
+            }
+        },
+    );
+
+    navbarFocusList[0].addEventListener('keydown', (e) => {
+        if (isExpanded() && e.key === 'Tab' && e.shiftKey) {
+            e.preventDefault();
+            HamburgerBtn.focus();
+        }
+    });
+};
+
+function throttle(fn, delay) {
+    let prevTime = 0;
+    return function (...args) {
+        const now = Date.now();
+
+        if (now - prevTime >= delay) {
+            fn.apply(this, args);
+            prevTime = now;
+        }
+    };
+}
+
+window.addEventListener('scroll', throttle(closeNav, 300));
+
+document.addEventListener('click', (e) => {
+    const isCLickedInside =
+        navbarWrapper.contains(e.target) ||
+        navbarList.contains(e.target) ||
+        HamburgerBtn.contains(e.target);
+
+    if (!isCLickedInside && isExpanded()) {
+        closeNav();
+    }
+});
+
+HamburgerBtn.addEventListener('click', toggleNav);
+window.addEventListener('resize', classReload);
+focusTab();
