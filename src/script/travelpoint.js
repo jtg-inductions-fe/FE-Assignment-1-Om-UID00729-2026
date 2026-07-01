503994d4b5fcfefc5d89e7d36757176f55b2599b
Om Handa <om.handa@joshtechnologygroup.com>
2026-07-02 01:27:15 +0530

    [OH_A1_03] TravelPoint section: Fixed reviews - BEM naming, cards component, icon fix, js rendering fix

--- src/script/travelpoint.js
+++ src/script/travelpoint.js
@@ -1,24 +1,31 @@
-let detailsCont = document.querySelector('.travelpoint__textSide__details');
+const detailsCards = document.querySelector('.travelpoint__details-cards');
 
-let data = [
-    { numbers: '500+', content: 'Holiday Package' },
-    { numbers: '100', content: 'Luxury Hotel' },
-    { numbers: '7', content: 'Premium Airlines' },
-    { numbers: '2k+', content: 'Happy Customers' },
+const statsData = [
+    { statValue: '500+', statContent: 'Holiday Package' },
+    { statValue: '100', statContent: 'Luxury Hotel' },
+    { statValue: '7', statContent: 'Premium Airlines' },
+    { statValue: '2k+', statContent: 'Happy Customers' },
 ];
 
-let renderData = () => {
-    let layout = data
-        .map((ele) => {
-            return `
-                <div class="travelpoint__textSide__details__cont">
-                    <h4>${ele.numbers}</h4>   
-                    <p>${ele.content}</p>
-                </div>
-            `;
-        })
-        .join('');
-    detailsCont.innerHTML += layout;
+const renderCards = () => {
+    statsData.map((ele) => {
+        const cardWrapper = document.createElement('div');
+        cardWrapper.className = 'card card--details travelpoint__card-wrapper';
+
+        const cardHeading = document.createElement('h4');
+        cardHeading.textContent = ele.statValue;
+
+        const cardPara = document.createElement('p');
+        cardPara.className = 'travelpoint__card-para';
+        cardPara.textContent = ele.statContent;
+        cardPara.addEventListener('click', () => {
+            cardPara.classList.toggle('travelpoint__card-para--show-text');
+        });
+
+        cardWrapper.appendChild(cardHeading);
+        cardWrapper.appendChild(cardPara);
+        detailsCards.appendChild(cardWrapper);
+    });
 };
 
-renderData();
+renderCards();
