fee7a5bb3b4d59a6887e0a9ad71e05456449859a
omhanda-jtg <om.handa@joshtechnologygroup.com>
2026-07-02 13:25:15 +0530

    OH_A1_03 Travelpoint section - Implemented travelpoint section. Implemented dynmically rendered cards and ensured responsiveness and pixel perfect.
    * [OH_A1_03] Travel Point section: Layout Created
    
    * [OH_A1_03] Travel Point Section - Pixel Perect layout design and detais rendering
    
    * [OH_A1_03] TravelPoint section: Added icons, blur effect and corrected Pixel Perfect
    
    * [OH_A1_03] TravelPoint section: Review fixes and margin corrections
    
    * [OH_A1_03] Travel Point Section - Pixel Perect layout design and detais rendering
    
    * [OH_A1_03] TravelPoint section: Added icons, blur effect and corrected Pixel Perfect
    
    * [OH_A1_03] TravelPoint section: mmargin and padding corrections
    
    * [OH_A1_03] TravelPoint section: Fixed reviews - BEM naming, cards component, icon fix, js rendering fix
    
    * [OH_A1_03] Fixed reviews: navbar scroll, card component naming
    
    * [OH_A1_03] Travel Point section: fixed reviews: naming

---
+++ src/script/travelpoint.js
@@ -0,0 +1,31 @@
+const detailsCards = document.querySelector('.travelpoint__details-cards');
+
+const statsData = [
+    { statValue: '500+', statContent: 'Holiday Package' },
+    { statValue: '100', statContent: 'Luxury Hotel' },
+    { statValue: '7', statContent: 'Premium Airlines' },
+    { statValue: '2k+', statContent: 'Happy Customers' },
+];
+
+const renderCards = () => {
+    statsData.map((ele) => {
+        const cardWrapper = document.createElement('div');
+        cardWrapper.className = 'card card--outline travelpoint__card-wrapper';
+
+        const cardHeading = document.createElement('h4');
+        cardHeading.textContent = ele.statValue;
+
+        const cardPara = document.createElement('p');
+        cardPara.className = 'travelpoint__card-para';
+        cardPara.textContent = ele.statContent;
+        cardPara.addEventListener('click', () => {
+            cardPara.classList.toggle('travelpoint__card-para--show-text');
+        });
+
+        cardWrapper.appendChild(cardHeading);
+        cardWrapper.appendChild(cardPara);
+        detailsCards.appendChild(cardWrapper);
+    });
+};
+
+renderCards();
