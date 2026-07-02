04d1223b3bb2974c293d5b641f4d1728b1b7ee82
Om Handa <om.handa@joshtechnologygroup.com>
2026-07-03 02:52:21 +0530

    [OH_A1_05] Footer section- Completed implementation, new component for links, added mixin for fontface

--- src/script/footer.js
+++ src/script/footer.js
@@ -1,20 +1,71 @@
-import { MOBILE } from './constants.js';
+import { TABLET } from './constants.js';
 
-let footerHead = document.querySelectorAll('.footer__details__cont__head');
+const footerBtns = document.querySelectorAll('.footer__details-btn');
 
-const isMobile = () => window.innerWidth <= MOBILE;
+const isMobile = () => window.innerWidth < TABLET;
 
-footerHead.forEach((el) => {
-    el.addEventListener('click', () => {
+const setState = (btn, isOpen) => {
+    const list = btn.nextElementSibling;
+    list.classList.toggle('footer__details-links--open', isOpen);
+    btn.setAttribute('aria-expanded', isOpen);
+
+    list.querySelectorAll('a').forEach((link) => {
+        link.tabIndex = isOpen ? 0 : -1;
+    });
+};
+
+const tabOrder = () => {
+    if (isMobile()) {
+        footerBtns.forEach((btn) => {
+            btn.tabIndex = 0;
+
+            const list = btn.nextElementSibling;
+            const isOpen = list.classList.contains(
+                'footer__details-links--open',
+            );
+
+            list.querySelectorAll('a').forEach((link) => {
+                link.tabIndex = isOpen ? 0 : -1;
+            });
+        });
+    } else {
+        footerBtns.forEach((btn) => {
+            btn.tabIndex = -1;
+
+            btn.nextElementSibling.querySelectorAll('a').forEach((link) => {
+                link.removeAttribute('tabindex');
+            });
+        });
+    }
+};
+
+footerBtns.forEach((btn) => {
+    btn.addEventListener('click', () => {
         if (!isMobile()) return;
-        let nextEle = el.nextElementSibling;
-        nextEle?.classList.toggle('footer__details__cont__text--open');
+
+        const isOpen = btn.nextElementSibling.classList.contains(
+            'footer__details-links--open',
+        );
+
+        footerBtns.forEach((button) => {
+            setState(button, false);
+        });
+
+        if (!isOpen) {
+            setState(btn, true);
+        }
+
+        tabOrder();
     });
 });
 
 window.addEventListener('resize', () => {
-    if (isMobile()) return;
-    document.querySelectorAll('.footer__details__cont__text').forEach((e) => {
-        e.classList.remove('footer__details__cont__text--open');
-    });
+    if (!isMobile()) {
+        footerBtns.forEach((btn) => {
+            setState(btn, false);
+        });
+    }
+    tabOrder();
 });
+
+tabOrder();
